42e39e0fda9e53cd06dee74208fd04d4058c7914
Larry Nicholson <56185254+nicholsonlarry@users.noreply.github.com>
2020-07-24 10:16:37 -0700

    Update show_ver.py
    Updated file output formats

--- show_ver.py
+++ show_ver.py
@@ -13,10 +13,10 @@ for line in f:
     print("Connecting to:", DEVICE)
     device = ConnectHandler(device_type="cisco_ios", ip=DEVICE, username=USERNAME, password=SECRET)
     output1 = device.send_command("show ver")
-    save_file = open("show ver"+str(DEVICE)+".txt","w")
+    save_file = open("show_ver_"+str(DEVICE)+".txt","w")
     save_file.write(output1)
     save_file.close()
     device.disconnect()
-    print(str(DEVICE)+"Complete")
+    print(str(DEVICE)+" Complete")
     # need to add else if or something to handle time out errors or bad username/password return
 print("Script Complete")
